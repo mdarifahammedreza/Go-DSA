170ae52d56cc3a3e94f1d9144991a702ec1ff468
mdarifahammedreza <mdarifahammedreza@gmail.com>
2026-07-15 02:50:53 +0600

    added some srting algo

--- sorting-algo/main.go
+++ sorting-algo/main.go
@@ -1,8 +1,40 @@
 // bubble sort practicing
 package main
-import "fmt"
-import "Go-DSA/data"
+
+import (
+	"Go-DSA/algo"
+	"Go-DSA/data"
+	"fmt"
+	"time"
+)
 func main(){
 	arry:= data.Arry
-	fmt.Println("bubble sort:",Bubble(arry))
+	// buble
+	start := time.Now()
+	fmt.Println("bubble sort -->",algo.Bubble(arry))
+	elapsed := time.Since(start)
+	fmt.Println("Bubble Sort Runtime:", elapsed)
+	// Selection
+	start = time.Now()
+	fmt.Println("Selection sort -->",algo.Selection(arry))
+	elapsed = time.Since(start)
+	fmt.Println("Selection Sort Runtime:", elapsed)
+
+	//insertion
+	start = time.Now()
+	fmt.Println("Insertion sort-->",algo.Insertion(arry))
+	elapsed  = time.Since(start)
+	fmt.Println("Insertion sort runtime:",elapsed)
+
+	//Defult
+	start = time.Now()
+	fmt.Println("defult sort-->",algo.Defult(arry))
+	elapsed  = time.Since(start)
+	fmt.Println("defult sort runtime:",elapsed)
+	// 	start = time.Now()
+	// elapsed  = time.Since(start)
+	// fmt.Println("Insertion sort runtime:",elapsed)
+
 }
+
+
